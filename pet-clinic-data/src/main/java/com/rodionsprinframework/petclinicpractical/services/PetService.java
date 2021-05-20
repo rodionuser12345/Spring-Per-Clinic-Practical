355c386ee18eb321968627dd0f1a6450c4a68608
rodionuser12345 <79834564+rodionuser12345@users.noreply.github.com>
2021-05-20 13:57:51 +0300

    Refactor Service Interfaces to Common Base Interfaces. Closes #11.

--- pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/PetService.java
+++ pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/PetService.java
@@ -2,13 +2,6 @@ package com.rodionsprinframework.petclinicpractical.services;
 
 import com.rodionsprinframework.petclinicpractical.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/VetService.java
+++ pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/VetService.java
@@ -1,15 +1,7 @@
 package com.rodionsprinframework.petclinicpractical.services;
 
-import com.rodionsprinframework.petclinicpractical.model.Owner;
 import com.rodionsprinframework.petclinicpractical.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Owner owner);
-
-    Set<Vet> findAll();
 }
