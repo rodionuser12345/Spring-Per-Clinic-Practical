0acf451b101b5d466b95d5a6811d61d4275cdda8
rodionuser12345 <79834564+rodionuser12345@users.noreply.github.com>
2021-05-22 15:28:52 +0300

    Create Specialty Map Service. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.rodionsprinframework.petclinicpractical.services;
+
+import com.rodionsprinframework.petclinicpractical.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
