a1f323f169830bb93b1851d8d920561d5f36c17d
rodionuser12345 <79834564+rodionuser12345@users.noreply.github.com>
2021-05-22 14:01:54 +0300

    Create Pet Type Map Service. Closes #29.

---
+++ pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.rodionsprinframework.petclinicpractical.services;
+
+import com.rodionsprinframework.petclinicpractical.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
