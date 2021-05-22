6738671e7bbe8ca288448c176490c308913722c8
rodionuser12345 <79834564+rodionuser12345@users.noreply.github.com>
2021-05-22 14:08:07 +0300

    Add Pet Types in with Bootstrap (startup). Closes #32.

--- pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/PetTypeService.java
+++ pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/PetTypeService.java
@@ -1,6 +1,8 @@
 package com.rodionsprinframework.petclinicpractical.services;
 
 import com.rodionsprinframework.petclinicpractical.model.PetType;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface PetTypeService extends CrudService<PetType,Long> {
 }
--- pet-clinic-web/src/main/java/com/rodionsprinframework/petclinicpractical/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/rodionsprinframework/petclinicpractical/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.rodionsprinframework.petclinicpractical.bootstrap;
 
 import com.rodionsprinframework.petclinicpractical.model.Owner;
+import com.rodionsprinframework.petclinicpractical.model.PetType;
 import com.rodionsprinframework.petclinicpractical.model.Vet;
 import com.rodionsprinframework.petclinicpractical.services.OwnerService;
+import com.rodionsprinframework.petclinicpractical.services.PetTypeService;
 import com.rodionsprinframework.petclinicpractical.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
-
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("John");
