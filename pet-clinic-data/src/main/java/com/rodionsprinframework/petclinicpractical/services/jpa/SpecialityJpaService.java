0977dd86a0b9b5f083555f315abb651087993788
rodionuser12345 <79834564+rodionuser12345@users.noreply.github.com>
2021-05-23 13:44:45 +0300

    Create Spring Data JPA Service for Vet Specialties. Closes #44.

---
+++ pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/jpa/SpecialityJpaService.java
@@ -0,0 +1,48 @@
+package com.rodionsprinframework.petclinicpractical.services.jpa;
+
+import com.rodionsprinframework.petclinicpractical.model.Speciality;
+import com.rodionsprinframework.petclinicpractical.repositories.SpecialityRepository;
+import com.rodionsprinframework.petclinicpractical.services.SpecialitiesService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class SpecialityJpaService implements SpecialitiesService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialityJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
