9b458dc13f7f7b8c51fb62f94b56bf9868b47d76
rodionuser12345 <79834564+rodionuser12345@users.noreply.github.com>
2021-05-23 14:30:46 +0300

    Create Map Based Visit Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.rodionsprinframework.petclinicpractical.services;
+
+import com.rodionsprinframework.petclinicpractical.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/services/map/VisitMapService.java
@@ -0,0 +1,41 @@
+package com.rodionsprinframework.petclinicpractical.services.map;
+
+import com.rodionsprinframework.petclinicpractical.model.Visit;
+import com.rodionsprinframework.petclinicpractical.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile("map")
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if(object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null ||
+                object.getPet().getOwner().getId() == null)
+            throw new RuntimeException("Invalid visit");
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
