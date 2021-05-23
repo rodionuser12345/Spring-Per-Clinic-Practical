74f8df8101d5704d88f41a21ee9151bf1ca2d904
rodionuser12345 <79834564+rodionuser12345@users.noreply.github.com>
2021-05-23 12:47:46 +0300

    Convert Vets to JPA Entities. Closes #37.

--- pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/model/Vet.java
+++ pet-clinic-data/src/main/java/com/rodionsprinframework/petclinicpractical/model/Vet.java
@@ -1,10 +1,17 @@
 package com.rodionsprinframework.petclinicpractical.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Vet extends Person{
+@Entity
+@Table(name = "Vets")
+public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
